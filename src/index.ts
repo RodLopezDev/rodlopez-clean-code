360bab0ef4f944f241081366386ee75ca2ff7b62
RodLopezDev <rodrigolopezdev@gmail.com>
2023-06-26 15:18:17 -0500

    feat: :sparkles: added component for loading

--- src/index.ts
+++ src/index.ts
@@ -2,7 +2,11 @@
 export { default as Repository } from "./repository/Repository";
 
 // STATE
+// Types
 export type { default as IRequest } from "./state/domain/IRequest";
 export type { default as IRequestMethods } from "./state/domain/IRequestMethods";
+// Hooks
 export { default as useRequest } from "./state/hooks/useRequest";
 export { default as useRequestState } from "./state/hooks/useRequestState";
+// Components
+export { default as RequestStateRenderer } from "./state/components/RequestStateRenderer";
